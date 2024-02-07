fa75c12f02d9ffe8d924b7a3c5dd9e5570e05758
Elyter <eliottbreg@gmail.com>
2024-02-07 11:07:16 +0100

    api with swagger

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const ping = require('./routes/ping.js');
 
 app.use('/', ping);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
